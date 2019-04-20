994a0b64f436de2c264fa3fa8231497bf01eb2b6
Dinkov <yoandinkov@gmail.com>
2019-04-20 15:27:11 +0200

    Refactoring.

--- src/data_retrieval/helpers/in_memory.py
+++ src/data_retrieval/helpers/in_memory.py
@@ -1,11 +1,14 @@
 import pickle
 from os import environ
 from src.data_retrieval.helpers import constant
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def get_articles():
     if constant.DB_FILE not in environ:
         raise Exception('Missing DB_FILE')
 
-    with open(constant.DB_FILE, 'rb') as f:
+    with open(environ[constant.DB_FILE], 'rb') as f:
         return pickle.load(f)
